e129ea494fe09c90bbff5d2b70adde982eb6a88c
Kenneth Stewart <kennetms@uci.edu>
2018-10-21 21:49:22 -0700

    Player will now respawn if they fall too far

---
+++ VGDCPlatformer/Assets/Respawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour {
+
+    public Transform mSpawnPoint;
+
+    public float mFallLimit;
+
+    // Use this for initialization
+    void Start ()
+    {
+        mSpawnPoint.position = new Vector3(transform.position.x, transform.position.y, 0.0f);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(transform.position.y < mFallLimit)
+        {
+            transform.position = new Vector3(mSpawnPoint.position.x, mSpawnPoint.position.y, 0.0f);
+        }
+	}
+}
