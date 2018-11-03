93507fd53e75b796365aaef90b6553064285d74b
jmcazare <jmcazare@uci.edu>
2018-11-03 11:34:15 -0700

    Pickup leads to 2nd level

--- VGDCPlatformer/Assets/OurGame/Scripts/TriggerP.cs
+++ VGDCPlatformer/Assets/OurGame/Scripts/TriggerP.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TriggerP : MonoBehaviour {
-
+    public string sceneName;
+    public int level = 1;
     private void OnTriggerEnter2D(Collider2D collision)
     {
 
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") && level == 1)
         {
-            //doesn't pick up anything yet
+            level = 2;
+            SceneManager.LoadScene(sceneName);
             Destroy(gameObject);
         }
     }
