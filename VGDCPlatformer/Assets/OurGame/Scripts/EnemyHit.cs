5fce4419c54b2e10e226c6f3e258ff2be6d4284f
Kenneth Stewart <kennetms@uci.edu>
2018-11-03 09:54:31 -0700

    Commiting to Pull

---
+++ VGDCPlatformer/Assets/OurGame/Scripts/EnemyHit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHit : MonoBehaviour {
+
+    public GameObject TriggerZone;
+    public TriggerFall a;
+    
+    void Start()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "environment")
+        {
+            a = TriggerZone.GetComponent<TriggerFall>();
+            a.fall = 2;
+            Destroy(gameObject);
+                
+        }
+    }
+}
+
+
---
+++ VGDCPlatformer/Assets/OurGame/Scripts/TriggerP.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerP : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            //doesn't pick up anything yet
+            Destroy(gameObject);
+        }
+    }
+}
