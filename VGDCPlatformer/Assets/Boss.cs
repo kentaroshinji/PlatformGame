ad0343cd2b0183f9aad8cd7ecd6356f31bd82a50
Fu Hui <fhui2@uci.edu>
2018-11-27 13:34:29 -0800

    Still working on it

---
+++ VGDCPlatformer/Assets/Boss.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour {
+    public Respawn respawn;
+    public Transform[] patrolPoints;
+    public float speed;
+    Transform currentPatrolPoint;
+    int currentPatrolIntex;
+
+    public Transform target;
+    public float chaseRange;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentPatrolIntex = 0;
+        currentPatrolPoint = patrolPoints[currentPatrolIntex];
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distanceToTarget = Vector3.Distance(transform.position, target.position);
+        if (distanceToTarget < chaseRange)
+        {
+
+            transform.Translate(Vector3.down * Time.deltaTime * speed);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "enemy")
+        {
+            Destroy(collision.gameObject);
+        }
+        else if (collision.gameObject.tag == "Player")
+        {
+            respawn.Reload();
+        }
+        else if (collision.gameObject.tag == "environment")
+        {
+           // Destroy(collision.gameObject);
+        }
+        else if (collision.gameObject.tag == "CrushBlock")
+        {
+            speed = 1;
+        }
+    }
+}
